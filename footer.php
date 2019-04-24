5bc24b980c12095400416b7890fcc16df4f8fb13
IreneTheiss <irene@IreneTheisss-iMac.local>
2019-04-24 08:30:00 +0200

    highlight parent page link when on child page

--- footer.php
+++ footer.php
@@ -13,23 +13,18 @@
         <div class="site-footer__col-two">
             <h3 class="headline headline--small">Explore</h3>
             <nav class="nav-list">
-            <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
-            </ul>
+            <?php wp_nav_menu( array(
+                'theme_location' => 'footerMenuOne'
+            )); ?>
             </nav>
         </div>
 
         <div class="site-footer__col-three">
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
-            <ul>
-                <li><a href="#">Legal</a></li>
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Careers</a></li>
-            </ul>
+            <?php wp_nav_menu( array(
+                'theme_location' => 'footerMenuTwo'
+            )); ?>
             </nav>
         </div>
         </div>
@@ -50,7 +45,13 @@
 
     </div>
 </footer>
-
 <?php wp_footer();?>
+<!-- Highlight parent page link when on child page -->
+<?php if (is_page()) {   ?>
+    <script type="text/javascript">
+        var el = document.getElementsByClassName("current-page-ancestor");
+        el[0].className += " " + 'current-menu-item';
+    </script>
+<?php } ?>
 </body>
 </html>
