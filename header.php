57690986ddf3c33d952acb7e16f981ad9a61f0bf
IreneTheiss <irene@IreneTheisss-iMac.local>
2019-04-24 08:30:48 +0200

    display menu

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes( ); ?> >
 <head>
-    <meta charset="utf-8" />
+    <meta charset="<?php bloginfo( 'charset' ) ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class( ); ?> >
     
     <header class="site-header">
         <div class="container">
@@ -15,13 +15,16 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
-            <ul>
+            <?php wp_nav_menu( array(
+                'theme_location' => 'mainMenu'
+            ) ); ?>
+           <!--  <ul>
                 <li><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
                 <li><a href="#">Blog</a></li>
-            </ul>
+            </ul> -->
             </nav>
             <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
