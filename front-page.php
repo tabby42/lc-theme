1e9438fd2ef2ebd28ba1f27f7968dbd5a865fb27
Irene Theiss <irene-rudel@gmx.at>
2019-04-25 14:52:40 +0200

    order events on frontpage by event date

--- front-page.php
+++ front-page.php
@@ -16,17 +16,32 @@
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
         <?php
+        $today = date('Ymd');
         $homepageEvents = new WP_Query( array(
             'posts_per_page' => 2,
-            'post_type' => 'event'
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => $today,
+                    'type' => 'numeric'
+                 )
+            )
         ) );
 
         while( $homepageEvents->have_posts() ) {
             $homepageEvents->the_post(); ?>
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="<?php the_permalink( ); ?>">
-                <span class="event-summary__month"><?php the_time( 'M' ) ?></span>
-                <span class="event-summary__day"><?php the_time( 'd' ) ?></span>  
+                <span class="event-summary__month"><?php 
+                    $eventDate = new DateTime( get_field( 'event_date' ) );
+                    echo $eventDate->format( 'M' );
+                ?></span>
+                <span class="event-summary__day"><?php echo $eventDate->format( 'd' ); ?></span>  
                 </a>
                 <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink( ); ?>"><?php the_title( ); ?></a></h5>
@@ -34,6 +49,7 @@
                 </div>
             </div>
         <?php }
+        wp_reset_postdata( );
         ?>
         
         <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link( 'event' ); ?>" class="btn btn--blue">View All Events</a></p>
@@ -63,7 +79,6 @@
                     </div>
                 </div>
         <?php }
-        wp_reset_postdata( );
         ?>
 
         <p class="t-center no-margin"><a href="<?php echo site_url( '/blog' ); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
