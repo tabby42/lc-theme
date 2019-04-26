78a3100e820244983bae03ba34e3555897df810d
Irene Theiss <irene-rudel@gmx.at>
2019-04-26 09:33:42 +0200

    adjust query for program archive

--- functions.php
+++ functions.php
@@ -19,8 +19,8 @@
     add_action( 'after_setup_theme', 'lc_features' );
 
     function lc_adjust_queries ( $query ) {
-        $today = date('Ymd');
         if ( !is_admin() && is_post_type_archive( 'event' ) && $query-> is_main_query() ) {
+            $today = date('Ymd');
             $query->set( 'posts_per_page', 4 );
             $query->set( 'meta_key', 'event_date' );
             $query->set( 'orderby', 'meta_value_num' );
@@ -33,6 +33,12 @@
                     'type' => 'numeric'
                  )
             ) );
+        }
+
+        if ( !is_admin() &&  is_post_type_archive( 'program' ) && $query-> is_main_query() ) {
+            $query->set( 'posts_per_page', -1 );
+            $query->set( 'orderby', 'title' );
+            $query->set( 'order', 'ASC' );
         }
     }
 
